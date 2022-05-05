5ab2ac237538464526015b3fa145c4583c43f821
Konomaster <thomasdovaletec@gmail.com>
2022-05-05 13:59:11 -0300

    commit inicial fork do @josaiasmoura/peer-network

---
+++ @josaiasmoura/peer-network/index.js
@@ -0,0 +1,10 @@
+/**
+ * @author Josaias Moura
+ */
+
+'use strict';
+
+const PeerNetwork = require('./lib/peer-network.js');
+
+module.exports = PeerNetwork;
+module.exports.PeerNetwork = PeerNetwork;
