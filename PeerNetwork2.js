ef82b84d1a030c92ddcf0c7afd06ca64e8b8dcd5
Konomaster <thomasdovaletec@gmail.com>
2022-07-18 09:30:30 -0300

    correcao no commit anterior

---
+++ PeerNetwork2.js
@@ -0,0 +1,25 @@
+const PeerNetwork = require("@josaiasmoura/peer-network")
+
+console.log("TATAHH")
+
+let peer = new PeerNetwork({group:"daze",password:"daze"},37712)
+
+peer.on("offline",(offPeer) => {
+	console.log("Peer esta offline agora! ",offPeer)
+}).on("message", (msg,from) => {
+	console.log("Voce recebeu mensagem!",from,msg.toString())
+}).on("online",(newPeer) => {
+	console.log("Novo peer online! ",newPeer)
+}).on("ready", () =>{
+	console.log("estou online!")
+	//jah=peer.__network.__peers
+	//console.log("Printando ",jah)
+}).on("warning",(err) => {
+	console.log("Alerta! ",err.message)
+}).on("startTest",()=>{
+	console.log("startTestChegou!")
+	peer.clearQueue()
+})
+
+peer.start(21202)
+
