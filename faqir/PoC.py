5d6b7b1e0ba9969469477ce3c990a93f65b4c435
Konomaster <thomasdovaletec@gmail.com>
2022-08-04 10:24:25 -0300

    tirei o tunel redundante, ver se vai dar erro unable to write to stream socket

--- faqir/PoC.py
+++ faqir/PoC.py
@@ -259,11 +259,11 @@ class PoC:
             if self.gonnaTest:
 
                 cmd = "socat -d -d udp-listen:"+str(self.tcp_local_port)+",reuseaddr tcp:localhost:"+str(self.udp_local_port)
-                cmd2 = "socat -d -d udp-listen:2000,reuseaddr udp:localhost:2000"
+                #cmd2 = "socat -d -d udp-listen:2000,reuseaddr udp:localhost:2000"
                 print(cmd)
                 #nao precisa de tunnel udp aqui pq ja vai receber no porto certo
                 tunnelTCP_UDP = Popen(cmd.split())
-                testTunnel=Popen(cmd2.split())
+                #testTunnel=Popen(cmd2.split())
 
                 serverRunning=True
                 # fechar o socket do peernetwork (ja foi feito pela biblioteca)
@@ -289,10 +289,6 @@ class PoC:
                     child.kill()
                 parent.kill()
 
-                parent = psutil.Process(testTunnel.pid)
-                for child in parent.children(recursive=True):
-                    child.kill()
-                parent.kill()
 
                 print("teste concluido com sucesso")
             else:
