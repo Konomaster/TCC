ef41431603342fec47f9f88ad41af03ca154c1a6
Konomaster <thomasdovaletec@gmail.com>
2022-08-08 19:48:06 -0300

    t

--- faqir/PoC.py
+++ faqir/PoC.py
@@ -112,7 +112,7 @@ class PoC:
             c = iperf3.Client()
             c.server_hostname = "localhost"
             c.port = self.iperf_port
-            c.blksize=800
+            #c.blksize=800
             # hole punch eh udp
             # deixar iperf determinar o tamanho do bloco
             #trocar esse valor depois pelo que der no tcp
@@ -139,8 +139,8 @@ class PoC:
                     return
 
                 sleep(2)
-                cmd = "socat -d -d -b 700000 tcp-listen:"+str(self.iperf_port)+",reuseaddr,reuseport udp:" + ipPeer + ":" + str(self.server_tcp_hole)+",sp="+str(self.tcp_local_port)
-                cmd2 = "socat -d -d -b 700000 tcp-listen:"+str(self.iperf_port)+",reuseaddr,reuseport udp:" + ipPeer + ":" + str(self.server_udp_hole)+",sp="+str(self.udp_local_port)
+                cmd = "socat -d -d -b 7000000 tcp-listen:"+str(self.iperf_port)+",reuseaddr,reuseport udp:" + ipPeer + ":" + str(self.server_tcp_hole)+",sp="+str(self.tcp_local_port)
+                cmd2 = "socat -d -d -b 7000000 tcp-listen:"+str(self.iperf_port)+",reuseaddr,reuseport udp:" + ipPeer + ":" + str(self.server_udp_hole)+",sp="+str(self.udp_local_port)
 
                 print(cmd)
                 print(cmd2)
@@ -214,8 +214,8 @@ class PoC:
                 self.s2.sendto(serverString.encode('utf-8'), ("0.0.0.0", 37711))
                 print(serverString)
 
-                cmd = "socat -d -d -b 700000 udp-listen:"+str(self.tcp_local_port)+",reuseaddr tcp:localhost:7000"
-                cmd2 = "socat -d -d -b 700000 udp-listen:"+str(self.udp_local_port)+",reuseaddr tcp:localhost:7000"
+                cmd = "socat -d -d -b 7000000 udp-listen:"+str(self.tcp_local_port)+",reuseaddr tcp:localhost:7000"
+                cmd2 = "socat -d -d -b 7000000 udp-listen:"+str(self.udp_local_port)+",reuseaddr tcp:localhost:7000"
                 print(cmd)
                 print(cmd2)
                 #nao precisa de tunnel udp aqui pq ja vai receber no porto certo
